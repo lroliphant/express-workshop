10f647dc892c8cd28968003b52d20eaffcf476ed
Lucy Oliphant <lucy_oliphant@yahoo.co.uk>
2016-07-03 20:04:58 +0100

    display blog posts below form

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
@@ -6,10 +7,32 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.post('/create-post', function(req, res){
-  // console.log('/create-post');
-  console.log(req.body);
-  res.redirect('/');
+
+  fs.readFile(__dirname + '/data/posts.json', function (error, file) {
+    if(error) {
+      res.send(error);
+    }
+
+    var parsedFile = JSON.parse(file.toString());
+
+    var currentTimestamp = Date.now();
+
+    parsedFile[currentTimestamp] = req.body.blogpost;
+
+    var jsonData = JSON.stringify(parsedFile);
+
+    fs.writeFile('data/posts.json', jsonData, function(error) {
+      console.log("done writing post!");
+      res.redirect('/');
+    });
+  });
 });
+
+app.get("/get-posts", function (req, res) {
+  res.sendFile(__dirname + '/data/posts.json');
+});
+
+
 
 app.listen(3000, function() {
   console.log("hi i'm ready to accept requests");
